0965f55f31d26c154a7ce90f15150d57171861f1
Damir Muratov <inso@MacBook-Pro-Damir.local>
2014-08-26 20:52:13 +0400

    second commit

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,70 +1,80 @@
-CREATE TABLE tbl_lookup
-(
-	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(128) NOT NULL,
-	code INTEGER NOT NULL,
-	type VARCHAR(128) NOT NULL,
-	position INTEGER NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# ************************************************************
+# Sequel Pro SQL dump
+# Версия 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Адрес: 127.0.0.1 (MySQL 5.6.15)
+# Схема: 33slona
+# Время создания: 2014-08-26 15:59:38 +0000
+# ************************************************************
 
-CREATE TABLE tbl_user
-(
-	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	username VARCHAR(128) NOT NULL,
-	password VARCHAR(128) NOT NULL,
-	email VARCHAR(128) NOT NULL,
-	profile TEXT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE tbl_post
-(
-	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	title VARCHAR(128) NOT NULL,
-	content TEXT NOT NULL,
-	tags TEXT,
-	status INTEGER NOT NULL,
-	create_time INTEGER,
-	update_time INTEGER,
-	author_id INTEGER NOT NULL,
-	CONSTRAINT FK_post_author FOREIGN KEY (author_id)
-		REFERENCES tbl_user (id) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE tbl_comment
-(
-	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	content TEXT NOT NULL,
-	status INTEGER NOT NULL,
-	create_time INTEGER,
-	author VARCHAR(128) NOT NULL,
-	email VARCHAR(128) NOT NULL,
-	url VARCHAR(128),
-	post_id INTEGER NOT NULL,
-	CONSTRAINT FK_comment_post FOREIGN KEY (post_id)
-		REFERENCES tbl_post (id) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE tbl_tag
-(
-	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(128) NOT NULL,
-	frequency INTEGER DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# Дамп таблицы tasks
+# ------------------------------------------------------------
 
-INSERT INTO tbl_lookup (name, type, code, position) VALUES ('Draft', 'PostStatus', 1, 1);
-INSERT INTO tbl_lookup (name, type, code, position) VALUES ('Published', 'PostStatus', 2, 2);
-INSERT INTO tbl_lookup (name, type, code, position) VALUES ('Archived', 'PostStatus', 3, 3);
-INSERT INTO tbl_lookup (name, type, code, position) VALUES ('Pending Approval', 'CommentStatus', 1, 1);
-INSERT INTO tbl_lookup (name, type, code, position) VALUES ('Approved', 'CommentStatus', 2, 2);
+DROP TABLE IF EXISTS `tasks`;
 
-INSERT INTO tbl_user (username, password, email) VALUES ('demo','$2a$10$JTJf6/XqC94rrOtzuF397OHa4mbmZrVTBOQCmYD9U.obZRUut4BoC','webmaster@example.com');
-INSERT INTO tbl_post (title, content, status, create_time, update_time, author_id, tags) VALUES ('Welcome!','This blog system is developed using Yii. It is meant to demonstrate how to use Yii to build a complete real-world application. Complete source code may be found in the Yii releases.
+CREATE TABLE `tasks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `date_to` date DEFAULT NULL,
+  `is_closed` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_tasks_user_id` (`user_id`),
+  CONSTRAINT `FK_tasks_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-Feel free to try this system by writing new posts and posting comments.',2,1230952187,1230952187,1,'yii, blog');
-INSERT INTO tbl_post (title, content, status, create_time, update_time, author_id, tags) VALUES ('A Test Post', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 2,1230952187,1230952187,1,'test');
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
 
-INSERT INTO tbl_comment (content, status, create_time, author, email, post_id) VALUES ('This is a test comment.', 2, 1230952187, 'Tester', 'tester@example.com', 2);
+INSERT INTO `tasks` (`id`, `user_id`, `text`, `date_to`, `is_closed`)
+VALUES
+	(22,1,'пусть все будет завтра','2014-08-27',1);
 
-INSERT INTO tbl_tag (name) VALUES ('yii');
-INSERT INTO tbl_tag (name) VALUES ('blog');
-INSERT INTO tbl_tag (name) VALUES ('test');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Дамп таблицы users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `login`, `password`, `name`)
+VALUES
+	(1,'insomnia.kzn@gmail.com','098f6bcd4621d373cade4e832627b4f6','Тестовый пользователь');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
